a94a2cdc88dbb54df31170472ab3a36f0468d1a3
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-01-29 15:11:49 +0100

    Build simple home page and register page with form

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+const WelcomePage = () => (
+  <Layout>
+    <h1 className="text-center mt-5">Welcome to Next.js</h1>
+    <img src="/static/mdb-react-small.png" alt="MDBReact" className="img-fluid d-block mx-auto mt-3" />
+  </Layout>
+);
+
+export default WelcomePage;
